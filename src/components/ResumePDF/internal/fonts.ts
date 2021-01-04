026e2e549ed26f95c78e88495c9337bb0af7088b
abulerforrest <alexander@buler-forrest.se>
2021-01-04 02:02:54 +0100

    fixed linking issue regarding fonts for pdf generator

--- src/components/ResumePDF/internal/fonts.ts
+++ src/components/ResumePDF/internal/fonts.ts
@@ -6,27 +6,27 @@ interface IExternalFonts {
 const externalFonts: IExternalFonts[] = [
     {
         family: "SourceCodeProLight",
-        src: "http://www.alexdev.se/assets/fonts/SourceCodePro-ExtraLight.ttf"     
+        src: "assets/fonts/SourceCodePro-ExtraLight.ttf"
     },
     {
         family: "Montserrat",
-        src: "http://www.alexdev.se/assets/fonts/Montserrat-Bold.ttf"     
+        src: "assets/fonts/Montserrat-Bold.ttf"
     },
     {
         family: "ArialBold",
-        src: "http://www.alexdev.se/assets/fonts/Arial-Bold.ttf"     
+        src: "assets/fonts/Arial-Bold.ttf"
     },
     {
         family: "ArialItalic",
-        src: "http://www.alexdev.se/assets/fonts/Arial-Italic.ttf"     
+        src: "assets/fonts/Arial-Italic.ttf"
     },
     {
         family: "Arial",
-        src: "http://www.alexdev.se/assets/fonts/Arial.ttf"     
+        src: "assets/fonts/Arial.ttf"
     },
     {
         family: "Nunito",
-        src: "http://www.alexdev.se/assets/fonts/NunitoSans-ExtraBold.ttf"     
+        src: "assets/fonts/NunitoSans-ExtraBold.ttf"
     }
 ]
 
